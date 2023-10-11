74aab46503635ab1022e012c01277c8ef8785f1d
TatiaSalukvadze <tatiasaluqvadze86@gmail.com>
2023-10-11 13:54:33 +0400

    adding react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [newp, setNew] = useState("false");
+  const [score, setScore] = useState("none");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {newp === "true" ? <ChildTwo score={score} /> : <ChildOne f={setScore} />}
+
+      <button style={styles.sub} onClick={() => setNew("true")}>
+        Submit
+      </button>
     </div>
   );
 }
 
+function ChildOne({ f }) {
+  const row = [];
+  for (let i = 1; i < 6; i++) {
+    row.push(<But inp={i} id={i} f={f} />);
+  }
+  return (
+    <div>
+      <button className="but">&#11088;</button>
+      <div style={{ paddingBottom: "70px" }}>
+        <h1 style={{ color: "white" }}>How Did we do?</h1>
+        <p style={{ color: "gray" }}>
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </p>
+      </div>
+      <div style={styles.flex}> {row}</div>
+    </div>
+  );
+}
+
+function ChildTwo({ score }) {
+  return (
+    <p style={{ color: "white" }}>
+      You selected {score}&#11088; out of 5&#11088;!!!!!!!!
+    </p>
+  );
+}
+
+function But({ inp, id, f }) {
+  function goor() {
+    f(id);
+    const elem = document.getElementById(id);
+    elem.style.backgroundColor = "orange";
+    for (let i = 1; i < 6; i++) {
+      if (i !== id) {
+        const elem = document.getElementById(i);
+        elem.disabled = true;
+      }
+    }
+  }
+  return (
+    <button id={id} className="but need" onClick={goor}>
+      {inp}
+    </button>
+  );
+  //
+}
+
+const styles = {
+  flex: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+  sub: {
+    width: "100%",
+    marginTop: "30px",
+    height: 50,
+    borderRadius: 50,
+    fontSize: "20px",
+    color: "white",
+    backgroundColor: "hsl(25, 97%, 53%)",
+  },
+};
 export default App;
